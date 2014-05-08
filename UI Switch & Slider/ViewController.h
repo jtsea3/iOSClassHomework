a16a4b163ae85587903e8958449ada353b62af8c
John <jt@Johns-MBP.local>
2014-05-08 15:39:18 -0700

    Homework 1

--- UI Switch & Slider/ViewController.h
+++ UI Switch & Slider/ViewController.h
@@ -10,4 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+
+- (IBAction)logMessage;
+- (IBAction)toggleChanged;
+- (IBAction)sliderMoved: (UISlider *)slider;
+
 @end
+
+
+
+/*
+ Create a new single view application in Xcode
+ Add a button in the storyboard that logs a message to the console when it is tapped.
+ Add a UISwitch to your view controller and NSLog a message to the console when it is toggled.
+ Add a UISlider to your view controller and NSLog a message to the console with the current slider value, but only when the value is greater than 0.5
+ */
+
--- UI Switch & Slider/ViewController.m
+++ UI Switch & Slider/ViewController.m
@@ -13,17 +13,69 @@
 @end
 
 @implementation ViewController
+{
+    float _currentValue;
+    float _newSliderValue;
+    float _oldSliderValue;
+    float _sliderChange;
+    
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _currentValue = self.slider.value;
+    _newSliderValue = 0;
+//    _oldSliderValue = 0;
+    _sliderChange = 0;
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) logMessage
+{
+    NSLog(@"The Button has been pressed");
+}
+- (IBAction) toggleChanged
+{
+    NSLog(@"The Toggle Switch has been flipped");
+}
+- (IBAction)sliderMoved: (UISlider *)slider;
+{
+    _newSliderValue = slider.value;
+
+    _sliderChange = ABS(_currentValue - _newSliderValue);
+    _currentValue = slider.value;
+
+
+    if (_sliderChange > .5)
+    {
+        NSLog(@"The Slider moved: %f", _sliderChange);
+        NSLog(@"The value of the slider is: %f", slider.value);
+    }
+    
+/*  Notes to myself
+Initialize _newSliderValue
+
+++Move the Slider++++++++++++
+    _sliderChange = abs val of _currentValue - _newSliderValue.
+    If _sliderChange is >= .5, NSLog _sliderChange, else do nothing
+
+ //    NSLog(@"The value of the slider is: %f", slider.value);
+ //    NSLog(@"The _currentValue is: %f", self.slider.value);
+ //    NSLog(@"The _newSliderValue is: %f", _newSliderValue);
+ //    NSLog(@"The _oldSliderValue is: %f",_oldSliderValue);
+ 
+ Had some trouble reporting out the variable values to be consistent with what the math was getting.  Talk to tchrs about this.
+*/
 }
 
 @end
